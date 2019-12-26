e4d439a2174e06d4eea55afedd5e30d72717138c
kritikmirg <57408020+kritikmirg@users.noreply.github.com>
2019-12-26 17:14:50 -0500

    Update AdventureDB.java

--- src/ad/AdventureDB.java
+++ src/ad/AdventureDB.java
@@ -11,39 +11,39 @@ public class AdventureDB
         int a1, a2, a3,a4;
         
 
-        a1 = Integer.parseInt(JOptionPane.showInputDialog(null, quest.Q1(), " Jail Escape", 3));
+        a1 = Integer.parseInt(JOptionPane.showInputDialog(null, quest.Q1(), "Cave of Wonder", 3));
         quest.setA1(a1);
         
         if (a1 == 2)
         {
-              a2 = Integer.parseInt(JOptionPane.showInputDialog(null, quest.Q2(), " Jail Escape", 3));
+              a2 = Integer.parseInt(JOptionPane.showInputDialog(null, quest.Q2(), "Cave of Wonder", 3));
               
              quest.setA2(a2);
              if (a2 == 2)
              {
-                  a3 = Integer.parseInt(JOptionPane.showInputDialog(null, quest.Q3(), " Jail Escape", 3));
+                  a3 = Integer.parseInt(JOptionPane.showInputDialog(null, quest.Q3(), "Cave of Wonder", 3));
              
                   quest.setA3(a3);
                 if (a3 == 3)
                 {
-                     a4 = Integer.parseInt(JOptionPane.showInputDialog(null, quest.Q4(), " Jail Escape", 3));
+                     a4 = Integer.parseInt(JOptionPane.showInputDialog(null, quest.Q4(), "Cave of Wonder", 3));
                        quest.setA4(a4);
-                       JOptionPane.showMessageDialog(null, quest.Message4(), " Jail Escape", 3);
+                       JOptionPane.showMessageDialog(null, quest.Message4(), "Cave of Wonder", 3);
                 }
                 else
                 {
-                    JOptionPane.showMessageDialog(null, quest.Message3(), " Jail Escape", 3);
+                    JOptionPane.showMessageDialog(null, quest.Message3(), "Cave of Wonder", 3);
                
                 }
                 }
              else
              {
-                  JOptionPane.showMessageDialog(null, quest.Message2(), " Jail Escape", 3);
+                  JOptionPane.showMessageDialog(null, quest.Message2(), "Cave of Wonder", 3);
                 } 
         }
         else
         {
-             JOptionPane.showMessageDialog(null, quest.Message1(), " Jail Escape", 3);
+             JOptionPane.showMessageDialog(null, quest.Message1(), "Cave of Wonder", 3);
             
         }
 
